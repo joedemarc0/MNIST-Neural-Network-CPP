199dda3893b2175950a3383d146e1abf70335323
demarcoj <demarcoj@umich.edu>
2025-08-25 17:47:14 -0400

    Added comments including param definitions and function purposes

--- src/network.h
+++ src/network.h
@@ -1,34 +1,6 @@
-/** 
- * At this stage, we need to:
- * 1. Build the Model
- *  Store layers in order std::vector<Layer>
- *  Provide a way to add layers
- * 
- * 2. Forward pass
- *  Input -> Each Layer -> Activation -> Output
- *  Return final output
- * 
- * 3. Backward pass
- *  Compute output error (loss gradient)
- *  Backpropagate through layers in reverse
- *  Update weights/biases using learning rate
- * 
- * 4. Training loop
- *  Iterate over epochs
- *  For each batch/sample
- *      Forward pass
- *      Compute loss
- *      Backpropagation
- *      Update weights/biases
- *  Track accuracy or loss for reporting
- * 
- * 5. Evaluation
- *  Run forward pass on test/validation set
- *  Compute accuracy/loss
- * 
- * 6. Model persistence
- *  Save weights/biases to file
- *  Load weights/biases from file
+/**
+ * Neural Network Class Implementation. Represents fully configured neural network, ready to train,
+ * predict, and evaluate data. Options to save and load models into /models/ folder
  */
 
 #ifndef NETWORK_H
@@ -44,10 +16,27 @@
 #include <stdexcept>
 
 
+/**
+ * @class Network
+ * Implements Neural Network class that handles training, predicting, evaluating,
+ * saving and loading model
+ */
 class Network {
     public:
+        /**
+         * @enum InitType
+         * Specifies which initialization method to use
+         */
         enum class InitType { NONE, RANDOM, XAVIER, HE };
 
+        /**
+         * Constructor
+         * @param input_size Size of input data (784,)
+         * @param learning_rate Eta in Gradient Descent, specifies how much weights should change by with each epoch
+         * @param actType Type of activation function to use for each HIDDEN layer. Output layer is SOFTMAX by default
+         * @param initType Type of weight initialization
+         * @param lossType Type of loss function to utilize
+         */
         Network(
             size_t input_size,
             double learning_rate,
@@ -56,6 +45,18 @@ class Network {
             Loss::LossType lossType
         );
 
+        /**
+         * Training loop function
+         * @param X Input image matrix, size (input_size, m), where m = 60000 for training set
+         * @param y Matrix of labels for image comparison, size (m, 1)
+         * @param epochs Number of epochs to iterate over
+         * @param batch_size Number of batches to split data into (using mini-batches technique)
+         * @param shuffle Whether to shuffle training set or not during training between epochs
+         * 
+         * Modifies weights and biases of Layer class objects using Gradient Descent
+         * Shuffles training Matrix X if shuffle=true, and splits into n batches if
+         * batch_size=n.
+         */
         void train(const Matrix& X, const Matrix& y,
                    size_t epochs,
                    size_t batch_size=0,
@@ -68,8 +69,23 @@ class Network {
         void loadModel(const std::string& filename);
     
     private:
+        /**
+         * Forward Propagation Function
+         * @param X Input matrix of size (784, m), where m is the size of the dataset
+         * @return A, Matrix that has undergone all layers and their activation functions, is of size (10, m)
+         * 
+         * Sends input matrix through all layers and activation functions and returns 10xm matrix 
+         * encoding the neural networks guess
+         */
         Matrix forward(const Matrix& X);
-        void backward(const Matrix& dA);
+
+        /**
+         * Backward Propagation Function
+         * @param y Labels Matrix, one-hot encoding all images
+         * 
+         * Cycles through layers backwards and updates layer weights and biases using layer.backward() function
+         */
+        void backward(const Matrix& y);
 
         std::vector<Layer> layers;
         size_t inputSize;
