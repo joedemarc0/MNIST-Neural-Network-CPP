3602b96721afd6f7771816e79d6c97fb9e158714
demarcoj <josephfrdemarco@gmail.com>
2026-03-24 14:46:22 -0400

    That didn't work

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,18 @@
 #include <opencv2/opencv.hpp>
 #include <chrono>
 
-using Clock = std::chrono::high_resolution_clock;
-using Ms = std::chrono::duration<double, std::milli>;
-
 
 int main() {
     try {
         MNISTDataset train_data = MNISTLoader::loadTrainingData();
         Network net = Network::loadModel("models/mnist_model1.txt");
-        
+
         if (!net.checkCompiled()) {
             std::cout << "Network Compile Error" << std::endl;
             return 1;
         }
 
         Visualizer::viewGrid(train_data, net);
-
-        // auto start = Clock::now();
-        // net.train(train_data, 10000, 10, 64, false, true);
-        // auto ms = Ms(Clock::now() - start).count();
     
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
